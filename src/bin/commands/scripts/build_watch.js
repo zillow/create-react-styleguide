b63b8c5d18002e1d34a2e8be8f00bf9d6db3f34c
Brian Stone <brians@zillowgroup.com>
2018-07-27 14:06:40 -0700

    Fix build scripts.

--- src/bin/commands/scripts/build_watch.js
+++ src/bin/commands/scripts/build_watch.js
@@ -1,7 +1,7 @@
 import { spawn } from 'child_process';
 import { babel } from '../../util/executables';
+import { BABEL_OPTIONS, BABEL_OPTIONS_ES } from '../../util/babel';
 import noop from '../../util/noop';
-import { BABEL_OPTIONS, BABEL_OPTIONS_ES } from './build';
 
 export default (argv, callback = noop) => {
     spawn(babel, [...BABEL_OPTIONS, '--watch'], {
---
+++ src/bin/util/babel.js
@@ -0,0 +1,2 @@
+export const BABEL_OPTIONS = ['src', '--out-dir', 'lib', '--copy-files'];
+export const BABEL_OPTIONS_ES = ['src', '--out-dir', 'es', '--copy-files'];
