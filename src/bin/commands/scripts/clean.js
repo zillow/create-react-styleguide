bc2e1bd927e2852d92603903bd75c7c51bdd598b
Brian Stone <brians@zillowgroup.com>
2018-07-27 17:32:43 -0700

    Fancy clean task.

--- src/bin/commands/scripts/clean.js
+++ src/bin/commands/scripts/clean.js
@@ -1,11 +1,21 @@
 import path from 'path';
 import runSeries from 'run-series';
 import rimraf from 'rimraf';
+import ora from 'ora';
 import noop from '../../util/noop';
 
 const currentDir = process.cwd();
 const dirs = ['lib', 'es', 'styleguide'];
 
 export default (argv, callback = noop) => {
-    runSeries(dirs.map(dir => cb => rimraf(path.join(currentDir, dir), {}, cb)), callback);
+    runSeries(
+        dirs.map(dir => cb => {
+            const spinner = ora(`Deleting ${dir}`).start();
+            rimraf(path.join(currentDir, dir), {}, () => {
+                spinner.succeed();
+                cb();
+            });
+        }),
+        callback
+    );
 };
