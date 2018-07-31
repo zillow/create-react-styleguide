2af5c17a32456427a4a48a1159bca47bf74157d5
Brian Stone <brians@zillowgroup.com>
2018-07-31 14:40:14 -0700

    Fix eslint for emotion template. Fix clean task to clean up coverage folder.

--- templates/emotion-styles/src/__tests__/index.test.js
+++ templates/emotion-styles/src/__tests__/index.test.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import toJson from 'enzyme-to-json';
-
+import { shallow } from 'enzyme';
 import Button, { StyledButton } from '../components/Button/Button';
 import Bootstrap from '../themes';
 
 describe('Themed Styled Button', () => {
     it('displays a button with requested text', () => {
-        const tree = shallow(
-            <StyledButton theme={Bootstrap}>Click Me!</StyledButton>
-        );
+        const tree = shallow(<StyledButton theme={Bootstrap}>Click Me!</StyledButton>);
         expect(toJson(tree)).toMatchSnapshot();
     });
 
     it('displays a secondary button with requested text', () => {
         const tree = shallow(
-            <StyledButton theme={Bootstrap} type="secondary">⊙_ʘ</StyledButton>
+            <StyledButton theme={Bootstrap} type="secondary">
+                ⊙_ʘ
+            </StyledButton>
         );
         expect(toJson(tree)).toMatchSnapshot();
     });
@@ -22,15 +22,15 @@ describe('Themed Styled Button', () => {
 
 describe('Themed Button', () => {
     it('displays a button with requested text', () => {
-        const tree = shallow(
-            <Button theme={Bootstrap}>Click Me!</Button>
-        );
+        const tree = shallow(<Button theme={Bootstrap}>Click Me!</Button>);
         expect(toJson(tree)).toMatchSnapshot();
     });
 
     it('displays a secondary button with requested text', () => {
         const tree = shallow(
-            <Button theme={Bootstrap} type="secondary">⊙_ʘ</Button>
+            <Button theme={Bootstrap} type="secondary">
+                ⊙_ʘ
+            </Button>
         );
         expect(toJson(tree)).toMatchSnapshot();
     });
