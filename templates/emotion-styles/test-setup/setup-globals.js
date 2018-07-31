8dab07dfc8402e9fa5c9395bcd52f92e6a51b7f8
Beryl Egerter <beryle@zillowgroup.com>
2018-07-31 10:53:33 -0700

    move emotion-styles to shallow tests

--- templates/emotion-styles/test-setup/setup-globals.js
+++ templates/emotion-styles/test-setup/setup-globals.js
@@ -1,6 +1,12 @@
 /* global expect */
 import { createSerializer } from 'jest-emotion';
 import * as emotion from 'emotion';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+// Configure shallow render
+Enzyme.configure({ adapter: new Adapter() });
+global.shallow = Enzyme.shallow;
 
 // Serialize emotion styles into snapshots
 expect.addSnapshotSerializer(createSerializer(emotion));
