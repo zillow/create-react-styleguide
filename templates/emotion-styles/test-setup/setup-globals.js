0ac1ed710889ad8d58b7cfebc50c1c06c043cfb6
Brian Stone <brians@zillowgroup.com>
2018-07-31 14:40:14 -0700

    Fix eslint for emotion template. Fix clean task to clean up coverage folder.

--- templates/emotion-styles/test-setup/setup-globals.js
+++ templates/emotion-styles/test-setup/setup-globals.js
@@ -1,12 +1,12 @@
 /* global expect */
+/* eslint-disable zillow/import/no-extraneous-dependencies */
 import { createSerializer } from 'jest-emotion';
 import * as emotion from 'emotion';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-// Configure shallow render
+// Configure enzyme to use React 16.x adapter
 Enzyme.configure({ adapter: new Adapter() });
-global.shallow = Enzyme.shallow;
 
 // Serialize emotion styles into snapshots
 expect.addSnapshotSerializer(createSerializer(emotion));
