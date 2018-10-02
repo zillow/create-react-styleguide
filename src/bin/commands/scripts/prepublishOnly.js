51168e35236d46a7b32f3b7bd669bd9598b6a1a2
Brian Stone <brians@zillowgroup.com>
2018-10-02 11:49:17 -0700

    Add eslint and test to the prepublish script.

--- src/bin/commands/scripts/prepublishOnly.js
+++ src/bin/commands/scripts/prepublishOnly.js
@@ -1,7 +1,17 @@
 import runSeries from 'run-series';
+import eslint from './eslint';
+import test from './test';
 import clean from './clean';
 import build from './build';
 
 export default (argv, callback) => {
-    runSeries([cb => clean(argv, cb), cb => build(argv, cb)], callback);
+    runSeries(
+        [
+            cb => eslint(argv, cb),
+            cb => test(argv, cb),
+            cb => clean(argv, cb),
+            cb => build(argv, cb),
+        ],
+        callback
+    );
 };
