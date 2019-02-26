3d87118f526d040954cc1dee7829dce6888502ec
Daniel Stockman <daniel.stockman@gmail.com>
2019-02-26 10:33:22 -0800

    fix: convert jest config into function declaration

--- src/js/createJestConfig.js
+++ src/js/createJestConfig.js
@@ -1,20 +1,20 @@
-const createJestConfig = () => ({
-    collectCoverage: false,
-    collectCoverageFrom: ['src/**/*.js', 'src/**/*.jsx'],
-    coveragePathIgnorePatterns: [],
-    coverageReporters: ['cobertura', 'html', 'text'],
-    coverageThreshold: {
-        global: {
-            branches: 75,
-            functions: 80,
-            lines: 70,
-            statements: 70,
+export default function createJestConfig() {
+    return {
+        collectCoverage: false,
+        collectCoverageFrom: ['src/**/*.js', 'src/**/*.jsx'],
+        coveragePathIgnorePatterns: [],
+        coverageReporters: ['cobertura', 'html', 'text'],
+        coverageThreshold: {
+            global: {
+                branches: 75,
+                functions: 80,
+                lines: 70,
+                statements: 70,
+            },
         },
-    },
-    testMatch: ['<rootDir>/**/__tests__/**/*.test.js', '<rootDir>/**/*.test.js'],
-    testEnvironment: 'node',
-    testPathIgnorePatterns: ['/node_modules/', '<rootDir>/lib/', '<rootDir>/es/'],
-    setupTestFrameworkScriptFile: '<rootDir>/jest.setup.js',
-});
-
-export default createJestConfig;
+        testMatch: ['<rootDir>/**/__tests__/**/*.test.js', '<rootDir>/**/*.test.js'],
+        testEnvironment: 'node',
+        testPathIgnorePatterns: ['/node_modules/', '<rootDir>/lib/', '<rootDir>/es/'],
+        setupTestFrameworkScriptFile: '<rootDir>/jest.setup.js',
+    };
+}
