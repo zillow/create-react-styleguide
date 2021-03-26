96f6f20eeb624b182c4732a241063e19de3e2c5a
Brian Stone <brians@zillowgroup.com>
2021-03-26 08:14:02 -0700

    feat: update default jest config
    BREAKING CHANGE: default `coverageThreshold` is now `100`, default `testEnvironment` is now `jsdom`,
    `clearMocks` is now enabled by default

--- templates/styled-components/jest.config.js
+++ templates/styled-components/jest.config.js
@@ -1,7 +1,6 @@
 const { createJestConfig } = require('create-react-styleguide');
 
 module.exports = createJestConfig({
-    /* your own config shallowly merged */
+    /* your own config shallowly merged here */
     setupFilesAfterEnv: ['jest-styled-components'],
-    testEnvironment: 'jsdom',
 });
