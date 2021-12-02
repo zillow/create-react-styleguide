de6174ec17d23be345647d6b8be811b26ddea08a
Kris Ellery <krise@zillowgroup.com>
2021-12-02 11:20:44 -0600

    chore: update dependencies, add commit lint, and update yargs usage

---
+++ commitlint.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+
+    rules: {
+        'body-case': [0],
+        'subject-case': [0],
+        'header-max-length': [0],
+        'type-enum': [
+            2,
+            'always',
+            [
+                // Conventional Commit types
+                'feat',
+                'feature',
+                'fix',
+                'perf',
+                'revert',
+                'docs',
+                'style',
+                'chore',
+                'refactor',
+                'test',
+                'build',
+                'ci',
+            ],
+        ],
+    },
+};
